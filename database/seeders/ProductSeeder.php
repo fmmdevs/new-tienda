eadb8ce6b103a18d50772f3ffa4679059e17ae48
fmmdevs <fmm.devs@gmail.com>
2024-06-08 01:06:36 +0200

    fixed seeders

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -17,28 +17,28 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Black One',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/black-1.jpeg'),
+            'image' => 'img/seeds/women/black-1.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'Black Two',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/black-2.jpeg'),
+            'image' => 'img/seeds/women/black-2.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'Black Three',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/black-3.jpeg'),
+            'image' => 'img/seeds/women/black-3.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'Black Four',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/black-4.jpeg'),
+            'image' => 'img/seeds/women/black-4.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
@@ -47,28 +47,28 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Grey One',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/grey-1.jpeg'),
+            'image' => 'img/seeds/women/grey-1.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'Grey Two',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/grey-2.jpeg'),
+            'image' => 'img/seeds/women/grey-2.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'Grey Three',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/grey-3.jpeg'),
+            'image' => 'img/seeds/women/grey-3.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'Grey Four',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/grey-4.jpeg'),
+            'image' => 'img/seeds/women/grey-4.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
@@ -77,28 +77,28 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'White One',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/white-1.jpeg'),
+            'image' => 'img/seeds/women/white-1.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'White Two',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/white-2.jpeg'),
+            'image' => 'img/seeds/women/white-2.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'White Three',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/white-3.jpeg'),
+            'image' => 'img/seeds/women/white-3.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
         Product::create([
             'name' => 'White Four',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/women/white-4.jpeg'),
+            'image' => 'img/seeds/women/white-4.jpeg',
             'price' => 10,
             'category_id' => 2
         ]);
@@ -107,28 +107,28 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Black One',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/black-1.jpeg'),
+            'image' => 'img/seeds/men/black-1.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'Black Two',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/black-2.jpeg'),
+            'image' => 'img/seeds/men/black-2.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'Black Three',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/black-3.jpeg'),
+            'image' => 'img/seeds/men/black-3.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'Black Four',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/black-4.jpeg'),
+            'image' => 'img/seeds/men/black-4.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
@@ -137,28 +137,28 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Gray One',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/grey-1.jpeg'),
+            'image' => 'img/seeds/men/grey-1.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'Gray Two',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/grey-2.jpeg'),
+            'image' => 'img/seeds/men/grey-2.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'Gray Three',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/grey-3.jpeg'),
+            'image' => 'img/seeds/men/grey-3.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'Gray Four',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/grey-4.jpeg'),
+            'image' => 'img/seeds/men/grey-4.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
@@ -167,28 +167,28 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'White One',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/white-1.jpeg'),
+            'image' => 'img/seeds/men/white-1.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'White Two',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/white-2.jpeg'),
+            'image' => 'img/seeds/men/white-2.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'White Three',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/white-3.jpeg'),
+            'image' => 'img/seeds/men/white-3.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
         Product::create([
             'name' => 'White Four',
             'description' => 'Bueno',
-            'image' => asset('img/seeds/men/white-4.jpeg'),
+            'image' => 'img/seeds/men/white-4.jpeg',
             'price' => 10,
             'category_id' => 1
         ]);
